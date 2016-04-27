72905158f70cf1bbadf03f2663cf85f1a7d6837e
Hjorthen <Hjorthensebastian@gmail.com>
2016-04-27 18:02:57 +0200

    Corrected test script format

--- src/test01917/Main.java
+++ src/test01917/Main.java
@@ -10,42 +10,82 @@ import connector01917.Connector;
 
 public class Main {
 	public static void main(String[] args) {
-		try { new Connector(); } 
-		catch (InstantiationException e) { e.printStackTrace(); }
-		catch (IllegalAccessException e) { e.printStackTrace(); }
-		catch (ClassNotFoundException e) { e.printStackTrace(); }
-		catch (SQLException e) { e.printStackTrace(); }
-		
+		try { 
+				new Connector(); 
+			} 
+		catch (InstantiationException e) 
+		{
+			e.printStackTrace(); 
+		}
+		catch (IllegalAccessException e) 
+		{ 
+			e.printStackTrace(); 
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace(); 
+			}
+		catch (SQLException e) {
+			e.printStackTrace(); 
+			}
 		System.out.println("Ansat nummer 3:");
 		MySQLAnsatDAO ans = new MySQLAnsatDAO();
-		try { System.out.println(ans.getAnsat("0909909009")); }
-		catch (DALException e) { System.out.println(e.getMessage()); }
-		
+		try { 
+				System.out.println(ans.getAnsat("0909909009")); 
+			}
+		catch (DALException e) { 
+				System.out.println(e.getMessage()); 
+			}
+
 		System.out.println("Indsaettelse af ny ansat med titel =  1");
 		AnsatDTO ansDTO = new AnsatDTO("0000000001","Don Juan","DJ","iloveyou",1);
-		try { ans.createAnsat(ansDTO); }
-		catch (DALException e) { System.out.println(e.getMessage()); }	
-		
+		try { 
+			ans.createAnsat(ansDTO); 
+			}
+		catch (DALException e) { 
+			System.out.println(e.getMessage()); 
+			}	
+
 		System.out.println("Ansat nummer 4:");
-		try { System.out.println(ans.getAnsat("0000000001")); }
-		catch (DALException e) { System.out.println(e.getMessage()); }
-		
+		try { 
+			System.out.println(ans.getAnsat("0000000001")); 
+			}
+		catch (DALException e) 
+		{ 
+			System.out.println(e.getMessage()); 
+			}
+
 		System.out.println("Opdatering af initialer for ansatte nummer 4");
 		ansDTO.setIni("DoJu");
-		try { ans.updateAnsat(ansDTO); }
-		catch (DALException e) { System.out.println(e.getMessage()); }
-		
+		try { 
+			ans.updateAnsat(ansDTO); 
+			}
+		catch (DALException e) { 
+			System.out.println(e.getMessage()); 
+			}
+
 		System.out.println("Ansat nummer 4:");
-		try { System.out.println(ans.getAnsat("0000000001")); }
-		catch (DALException e) { System.out.println(e.getMessage()); }
-		
+		try { 
+			System.out.println(ans.getAnsat("0000000001")); 
+			}
+		catch (DALException e) { 
+			System.out.println(e.getMessage()); 
+			}
+
 		System.out.println("Alle ansatte:");
-		try { System.out.println(ans.getAnsatList()); }
-		catch (DALException e) { System.out.println(e.getMessage()); }
-		
+		try { 
+			System.out.println(ans.getAnsatList()); 
+			}
+		catch (DALException e) { 
+			System.out.println(e.getMessage()); 
+			}
+
 		System.out.println("Ansat nummer 5:");
-		try { System.out.println(ans.getAnsat("0000000005")); }
-		catch (DALException e) { System.out.println(e.getMessage()); }		
-		
+		try { 
+			System.out.println(ans.getAnsat("0000000005")); 
+			}
+		catch (DALException e) { 
+			System.out.println(e.getMessage()); 
+			}		
+
 	}
 }
