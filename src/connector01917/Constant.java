8d7a7d22ac66deb255863d69a2174cc214b7aad9
Thomas Bech Madsen <thomasbm@live.dk>
2016-05-11 13:20:10 +0200

    Changed constant to fit new user

--- src/connector01917/Constant.java
+++ src/connector01917/Constant.java
@@ -7,9 +7,9 @@ public abstract class Constant
 {
 	public static final String
 		server					= "localhost",  // database-serveren
-		database				=  "projekt",  //"jdbcdatabase", // navnet paa din database = dit studienummer
-		username				= "root", // dit brugernavn = dit studienummer 
-		password				= ""; // dit password som du har valgt til din database
+		database				=  "store",  //"jdbcdatabase", // navnet paa din database = dit studienummer
+		username				= "User", // dit brugernavn = dit studienummer 
+		password				= "qwerty"; // dit password som du har valgt til din database
 	
 	public static final int
 		port					= 3306;
